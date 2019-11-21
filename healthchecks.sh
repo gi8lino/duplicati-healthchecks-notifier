2b8be0a7c24aba7c8e7af798af3f8704f46f9d7a
giotto <giottolino@gmail.com>
2019-11-21 13:30:29 +0100

    Update healthchecks.sh

--- healthchecks.sh
+++ healthchecks.sh
@@ -1,36 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 delcare -A RESULTS
 delcare -A OPERATIONS
-declare -A DUPLICATI
+
+
+function jsonValue() {
+    local KEY=$1
+    local num=$2
+    awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
+}
+
+if [ -z "$1" ]; then
+    echo -e "no healthcheck ID passed to the script! exit"
+    exit 1
+fi
 
 RESULTS=( "Error" "Warning" "Fatal" "Unknown" "Success" )
 OPERATIONS=( "Backup" "Restore")
 
-HEALTHCHECKS_URL="https://healthchecks.giottolino.ch/ping/"
-SLACK_URL
+HEALTHCHECKS_URL="https://healthchecks.giottolino.ch"
 
-BACKUP_JOBS= ( ["opt"]="id" )
-
-
-slack() (
-    local _text=$1
-    result=(curl -fsS --retry 3 ${SLACK_URL}> /dev/null)
-)
 
 if [[ " ${RESULTS[@]} " =~ " ${DUPLICATI_PARSED_RESULT} " ]] && [[ " $OPERATIONS[@]} " "${DUPLICATI_OPERATIONNAME} " ]]; then
 
-    ID=${BACKUP_JOBS[${DUPLICATI__backup_name}]}
-    if [ ! -z "ID" ]; then
-        echo "no id for backupjob '${DUPLICATI__backup_name}' found"
-    fi
-    URL=${HEALTHCHECKS_URL}${ID}
+    HEALTHCHECKS_CHECKS=$(curl --header "X-Api-Key: your-api-key" HEALTHCHECKS_URL/api/v1/checks/)
+
+    PING_URL=$(${HEALTHCHECKS_CHECKS} | | jsonValue ${DUPLICATI__backup_name} 1 )
 
     if [ ${DUPLICATI_PARSED_RESULT} != "Success" ]; then
-        URL=${URL}/fail
+        URL="${URL}/fail"
     fi
+    result=$(curl -fsS --retry 3 ${URL}> /dev/null)
 
-    result=(curl -fsS --retry 3 URL> /dev/null)
-
+    if [ ! ${result} == "ok" ]; then
+        echo -e "cannot update healthchecks!"
+    fi
 fi
 
 exit 0
