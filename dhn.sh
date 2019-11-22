ba9e5532d0ec77bd7b0b2d2a0e89a04f5b8645ab
gi8 <giottolino@gmail.com>
2019-11-22 01:12:19 +0100

    update

--- dhn.sh
+++ dhn.sh
@@ -28,30 +28,28 @@ function ShowHelp {
            "" \
            "Environment variables:" \
            "You can also use environment variables to set the healthchecks url and token." \
-           "HC_URL                             healthchecks url" \
-           "HC_TOKEN                           healthchecks API Access ('read-only' token does not work!)" \
+           "HC_URL                              healthchecks url" \
+           "HC_TOKEN                            healthchecks API Access ('read-only' token does not work!)" \
 		   "" \
 		   "created by gi8lino (2019)" \
-           "https://github.com/gi8lino/duplicati-healthchecks"
+           "https://github.com/gi8lino/duplicati-healthchecks-notifier"
 	exit 0
 }
 
 # level: level of log entry
 # text: message of log entry
-# logpath: path to logfile. if set, the log entry will be saved in this file
 function log() {
     local _level=$1
     local _text=$2
-    local _logpath=$3
 
     local _current=`date '+%Y-%m-%d %H:%M:%S'`
 
-    if [ ${_level} == "DEBUG" ] && [ ! -n "${DEBUG}" ]; then
+    if [ ${_level} == "DEBUG" ] && [ -z "${DEBUG}" ]; then
         return
     fi
 
-    if [ ${_logpath} ];then
-        echo "${_current}; ${_level}; ${_text}" >> "${_logpath}"
+    if [ -n "${LOG_FILE}" ];then
+        echo "${_current}; ${_level}; ${_text}" >> "${LOG_FILE}"
     else
         echo "${_current}; ${_level}; ${_text}"
     fi
@@ -103,65 +101,64 @@ while [[ $# -gt 0 ]];do
 done
 
 if [ -n "${LOG_FILE}" ];then
-    to_file=$LOG_FILE  # set path to logfile
-    log "DEBUG" "log to file '${to_file}' enabled" $to_file
+    log "DEBUG" "log to file '${LOG_FILE}' enabled"
 fi
 
 if [ ! -n "${HEALTHCHECKS_URL}" ]; then
     HEALTHCHECKS_URL=$(printenv HC_URL)
     if [ ! -n "${HEALTHCHECKS_URL}" ]; then
-        log "ERROR" "healthchecks url not found in script start parameter (-u|--url) nor in environment (HC_URL)" $to_file
+        log "ERROR" "healthchecks url not found in script start parameter (-u|--url) nor in environment (HC_URL)"
         exit 1
     fi
-    log "DEBUG" "get healthchecks url '${HEALTHCHECKS_URL}' from environmentvariable 'HC_URL'" $to_file
+    log "DEBUG" "get healthchecks url '${HEALTHCHECKS_URL}' from environmentvariable 'HC_URL'"
 else
-    log "DEBUG" "get healthchecks url '${HEALTHCHECKS_URL}' from start argument" $to_file
+    log "DEBUG" "get healthchecks url '${HEALTHCHECKS_URL}' from start argument"
 fi
 
 if [ ! -n "${TOKEN}" ]; then
     TOKEN=$(printenv HC_TOKEN)
     if [ ! -n "${TOKEN}" ]; then
-        log "ERROR" "healthchecks token not found in script start parameter nor in environment (HC_TOKEN)" $to_file
+        log "ERROR" "healthchecks token not found in script start parameter nor in environment (HC_TOKEN)"
         exit 1
     fi
-    log "DEBUG" "get healthchecks token '${TOKEN}' from environmentvariable 'HC_TOKEN'" $to_file
+    log "DEBUG" "get healthchecks token '${TOKEN}' from environmentvariable 'HC_TOKEN'"
 else
-    log "DEBUG" "get healthchecks token '${TOKEN}' from start argument" $to_file
+    log "DEBUG" "get healthchecks token '${TOKEN}' from start argument"
 fi
 
 if [ -n "${JQ_PATH}" ]; then
     if [ ! -f "${JQ_PATH}" ]; then
-        log "ERROR" "path to jq '${JQ_PATH}' does not exists" $to_file
+        log "ERROR" "path to jq '${JQ_PATH}' does not exists"
         exit 1
     fi
-    log "DEBUG" "use 'jq' from '${JQ_PATH}'" $to_file
+    log "DEBUG" "use 'jq' from '${JQ_PATH}'"
 else
     if [ ! -x "$(command -v jq)" ]; then
-        log "ERROR" "'jq' is not installed! please install 'jq' or download binary and add the path as start parameter (-j|--jq-path)" $to_file
+        log "ERROR" "'jq' is not installed! please install 'jq' or download binary and add the path as start parameter (-j|--jq-path)"
         exit 1
     fi
     JQ_PATH="jq"
-    log "DEBUG" "use installed 'jq'" $to_file
+    log "DEBUG" "use installed 'jq'"
 fi
 
-log "DEBUG" "duplicati result is '${DUPLICATI__PARSED_RESULT}'" $to_file
-log "DEBUG" "duplicati operation is '${DUPLICATI__OPERATIONNAME}'" $to_file
-log "DEBUG" "duplicati backup name is '${DUPLICATI__backup_name}'" $to_file
+log "DEBUG" "duplicati result is '${DUPLICATI__PARSED_RESULT}'"
+log "DEBUG" "duplicati operation is '${DUPLICATI__OPERATIONNAME}'"
+log "DEBUG" "duplicati backup name is '${DUPLICATI__backup_name}'"
 
 if [[ ! " ${RESULTS[@]} " =~ " ${DUPLICATI__PARSED_RESULT} " ]]; then
-    log "DEBUG" "'${DUPLICATI__PARSED_RESULT}' is not a valid result (valid: $(IFS=\| ; echo "${RESULTS[*]}"))" $to_file
+    log "DEBUG" "'${DUPLICATI__PARSED_RESULT}' is not a valid result (valid: $(IFS=\| ; echo "${RESULTS[*]}"))"
     exit 1
 fi
 
 if [[ "${DUPLICATI__OPERATIONNAME}" != "Backup" ]]; then
-    log "DEBUG" "'${DUPLICATI__OPERATIONNAME}' is not a wanted operation (valid: Backup)" $to_file
+    log "DEBUG" "'${DUPLICATI__OPERATIONNAME}' is not a wanted operation (valid: Backup)"
     exit 1
 fi
 # get healthcheck entries
 HEALTHCHECKS_CHECKS=$(curl -fsS --retry 3 --header "X-Api-Key: ${TOKEN}" "${HEALTHCHECKS_URL%/}/api/v1/checks/")
 
 if [ ! -n "${HEALTHCHECKS_CHECKS}" ] || [ "${HEALTHCHECKS_CHECKS}" == "null" ]; then
-    log "ERROR" "cannot receive list of existing checks" $to_file
+    log "ERROR" "cannot receive list of existing checks"
     exit 1
 fi
 
@@ -169,7 +166,7 @@ fi
 PING_URL=$(echo "${HEALTHCHECKS_CHECKS}" | ${JQ_PATH} -r ".checks[] | select(.name == \"${DUPLICATI__backup_name}\").ping_url")
 
 if [ -z "${PING_URL}" ] || [ "${PING_URL}" == "null" ]; then
-    log "ERROR" "cannot evaluate ping url for '${DUPLICATI__backup_name}'" $to_file
+    log "ERROR" "cannot evaluate ping url for '${DUPLICATI__backup_name}'"
     exit 1
 fi
 
@@ -177,15 +174,15 @@ if [ ${DUPLICATI__PARSED_RESULT} != "Success" ]; then
     PING_URL="${PING_URL}/fail"
 fi
 
-log "DEBUG" "get 'ping_url' '${PING_URL}'" $to_file
+log "DEBUG" "get 'ping_url' '${PING_URL}'"
 
 result=$(curl -fsS --retry 3 "${PING_URL}")
-log "DEBUG" "healthchecks retuned '${result}'" $to_file
+log "DEBUG" "healthchecks retuned '${result}'"
 
 if [ "${result}" != "OK" ]; then
-    log "ERROR" "cannot update healthchecks! healthchecks returned: '${result}''" $to_file
+    log "ERROR" "cannot update healthchecks! healthchecks returned: '${result}''"
     exit 1
 fi
-log "INFO" "healthcheck for Duplicati job '${DUPLICATI__backup_name}' successfully updated (backup status: ${DUPLICATI__PARSED_RESULT})" $to_file
+log "INFO" "healthcheck for Duplicati job '${DUPLICATI__backup_name}' successfully updated (backup status: ${DUPLICATI__PARSED_RESULT})"
 
 exit 0
